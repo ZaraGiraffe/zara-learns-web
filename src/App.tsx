97a21bb635760ebfe6edde470b72f70bc978fc5f
zahar <znaumets@gmail.com>
2025-12-03 23:51:58 +0200

    start building upper panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
 
+function HeaderPanel() {
+    return <div className="header-panel">
+        <img srcSet="/public/logo.svg" alt="logo" className="logo-image" />
+        <div className="header-central-buttons">
+            <div>Home</div>
+            <div>Catalog</div>
+            <div>About</div>
+            <div>Contact</div>
+        </div>
+        <div className="profile">Sign in</div>
+    </div>
+}
+
 function App() {
-  return (
-    <>
-      <p> Hello </p>
-    </>
-  )
+    return (
+        <>
+            <HeaderPanel />
+        </>
+    )
 }
 
 export default App
