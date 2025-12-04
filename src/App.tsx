ceb89728580538badb5c5824ca92bcea92261f1d
zahar <znaumets@gmail.com>
2025-12-04 22:05:21 +0200

    Add simple styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 
 function HeaderPanel() {
     return <div className="header-panel">
-        <img srcSet="/public/logo.svg" alt="logo" className="logo-image" />
+        <img srcSet="/logo.svg" alt="logo" className="logo-image" />
         <div className="header-central-buttons">
             <div>Home</div>
             <div>Catalog</div>
@@ -13,10 +13,25 @@ function HeaderPanel() {
     </div>
 }
 
+function PageContent() {
+    return <div className="page-content"><MockContent /></div>
+}
+
+function MockContent() {
+    let text = "";
+    for (let i = 0; i < 1000; i++) {
+        text += "Lorem ipsum";
+    }
+    return <p>
+        {text}
+    </p>
+}
+
 function App() {
     return (
         <>
             <HeaderPanel />
+            <PageContent />
         </>
     )
 }
