888a57f04da2c1f0374512539cb7f555e6f5360f
zahar <znaumets@gmail.com>
2026-01-04 10:09:57 +0200

    add layout

--- frontend-admin/src/App.tsx
+++ frontend-admin/src/App.tsx
@@ -1,7 +1,34 @@
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  return <>Hello World</>
+  return <>
+    <div className='main-page'>
+      <LeftPanel />
+      <div className='main-panel'>
+        <Header />
+        <Content />
+      </div>
+    </div>
+  </>
 }
 
 export default App
+
+function LeftPanel() {
+  return <div className='left-panel'>
+
+  </div>
+}
+
+function Header() {
+  return <div className='header'>
+    
+  </div>
+}
+
+function Content() {
+  return <div className='content'>
+    
+  </div>
+}
