71a4b463d9b205392979392f342d2cd43ed308cf
zahar <znaumets@gmail.com>
2026-01-05 11:16:57 +0200

    add transform

--- frontend-admin/src/App.tsx
+++ frontend-admin/src/App.tsx
@@ -23,9 +23,17 @@ function LeftPanel() {
 
 function Header() {
   return <div className='header'>
-    <img src='./sidebar.svg' alt='sidebar' />
+    <img className='sidebar-icon' src='./sidebar.svg' alt='sidebar' onClick={handleSidebarIconClick} />
     <img src='./person.svg' alt='person' />
   </div>
+}
+
+function handleSidebarIconClick() {
+  if (document.querySelector('.main-page')?.classList.contains('sidebar-closed')) {
+    document.querySelector('.main-page')?.classList.remove('sidebar-closed');
+  } else {
+    document.querySelector('.main-page')?.classList.add('sidebar-closed');
+  }
 }
 
 function Content() {
